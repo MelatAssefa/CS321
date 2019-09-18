f558442f84e92ea531552cc1002e45f8da96db65
Melat <49922671+MelatAssefa@users.noreply.github.com>
2019-09-18 17:37:12 -0400

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,1 +1,24 @@
-Hello World
+</doctype html>
+<html lang="en">
+  <head>
+    <title>Melat Filmography</title>
+    <meta charset=”utf-8”>
+  </head>
+  <body>
+    <h1>Melat's Film Guide</h1>
+    
+    <ul><a href = "home.html"> Home </a></ul>
+    <ul><a href = "home.html"> Favorite Films </a></ul>
+    <ul><a href = "home.html"> **** </a></ul>
+    <ul><a href = "home.html"> *** </a></ul>
+    <ul><a href = "home.html"> ** </a></ul>
+    <ul><a href = "home.html"> * </a></ul>
+    
+    <a href="http://www.google.com">  This</a> is within the body tag.
+    
+   <p> In Hollywood, actor James Franco is throwing a party with a slew of celebrity pals. Among those in attendance are his buddies Jonah Hill, Seth Rogen, Jay Baruchel, Danny McBride and Craig Robinson. Suddenly, an apocalypse of biblical proportions erupts, causing untold carnage among Tinseltown's elite and trapping Franco's party in his home. As the world they knew disintegrates outside, cabin fever and dwindling supplies threaten to tear the six friends apart. </p>
+    
+   <img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/36/This-is-the-End-Film-Poster.jpg/220px-This-is-the-End-Film-Poster.jpg">
+  </body>
+
+</html>
