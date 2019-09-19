28c9e175de1348245b613a47d139e5569bbd7468
Melat <49922671+MelatAssefa@users.noreply.github.com>
2019-09-18 20:23:00 -0400

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,24 +1,42 @@
-</doctype html>
+<!-- Author: Melat A Ali -->
+<!-- Project: Assignment 2 -->
+
+<!doctype html>
 <html lang="en">
+
   <head>
-    <title>Melat Filmography</title>
+
+    <title>Melat's Film Guide</title>
     <meta charset=”utf-8”>
+    <link rel="stylesheet" href="stylesheet.css">
+    <link rel='icon' href='favicon.ico' type='image/x-icon'/ >
+
   </head>
+
   <body>
-    <h1>Melat's Film Guide</h1>
-    
-    <ul><a href = "home.html"> Home </a></ul>
-    <ul><a href = "home.html"> Favorite Films </a></ul>
-    <ul><a href = "home.html"> **** </a></ul>
-    <ul><a href = "home.html"> *** </a></ul>
-    <ul><a href = "home.html"> ** </a></ul>
-    <ul><a href = "home.html"> * </a></ul>
-    
-    <a href="http://www.google.com">  This</a> is within the body tag.
-    
-   <p> In Hollywood, actor James Franco is throwing a party with a slew of celebrity pals. Among those in attendance are his buddies Jonah Hill, Seth Rogen, Jay Baruchel, Danny McBride and Craig Robinson. Suddenly, an apocalypse of biblical proportions erupts, causing untold carnage among Tinseltown's elite and trapping Franco's party in his home. As the world they knew disintegrates outside, cabin fever and dwindling supplies threaten to tear the six friends apart. </p>
-    
-   <img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/36/This-is-the-End-Film-Poster.jpg/220px-This-is-the-End-Film-Poster.jpg">
+
+    <div class="header">
+        <h1 class="title1">Melat's Film Guide</h1>
+    </div>
+
+    <div class="menu">
+      <ul>
+      <li><a href="index.html">Home</a></li>
+      <li class="dropdown">
+        <a href="favfilms.html" class="dropbtn">Favorite Movies</a>
+        <div class="dropdown-content">
+          <a href="thisistheend.html">This Is The End</a>
+          <a href="#">True Romance</a>
+          <a href="#">The Interview</a>
+        </div>
+      </li>
+      <li><a href = "fourstars.html"> &#9734&#9734&#9734&#9734 </a></li>
+      <li><a href = "threestars.html"> &#9734&#9734&#9734 </a></li>
+      <li><a href = "twostars.html"> &#9734&#9734 </a></li>
+      <li><a href = "onestar.html"> &#9734 </a></li>
+    </ul>
+    </div>
+
   </body>
 
 </html>
