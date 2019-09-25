77765259faa857202f9c0146027de310ad5e42aa
Melat <49922671+MelatAssefa@users.noreply.github.com>
2019-09-25 16:25:00 -0400

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,41 +1,96 @@
-<!-- Author: Melat A Ali -->
-<!-- Project: Assignment 2 -->
-
 <!doctype html>
+
 <html lang="en">
 
   <head>
+
+    <!-- Author: Melat A Ali -->
+    <!-- Project: Assignment 2 -->
 
     <title>Melat's Film Guide</title>
     <meta charset=”utf-8”>
     <link rel="stylesheet" href="stylesheet.css">
     <link rel='icon' href='favicon.ico' type='image/x-icon'/ >
 
+    <!--Favicon: https://www.101computing.net/html-how-to-add-a-favicon/
+    https://www.iconfinder.com/icons/1298806/action_design_movie_process_round_icon-->
+
   </head>
 
   <body>
 
     <div class="header">
-        <h1 class="title1">Melat's Film Guide</h1>
+        <h1 class="title">Melat's Film Guide</h1>
     </div>
 
     <div class="menu">
       <ul>
       <li><a href="index.html">Home</a></li>
-      <li class="dropdown">
-        <a href="favfilms.html" class="dropbtn">Favorite Movies</a>
-        <div class="dropdown-content">
-          <a href="thisistheend.html">This Is The End</a>
-          <a href="#">True Romance</a>
-          <a href="#">The Interview</a>
+      <li class="drop1">
+        <a href="comedy.html" class="drop2">Comedy</a>
+        <div class="drop3">
+          <a href="movies\thisistheend.html">This Is The End</a>
+          <a href="movies\theinterview.html">The Interview</a>
+          <a href="movies\thedictator.html">The Dictator</a>
+          <a href="movies\amelie.html">Amelie</a>
+        </div>
+      <li class="drop1">
+        <a href="drama.html" class="drop2">Drama</a>
+        <div class="drop3">
+          <a href="movies\500daysofsummer.html">500 Days of Summer</a>
+          <a href="movies\thewolfofwallstreet.html">The Wolf of Wall Street</a>
+          <a href="movies\panslabyrinth.html">Pan's Labyrinth</a>
+          <a href="movies\underthetuscansun.html">Under The Tuscan Sun</a>
+
         </div>
       </li>
-      <li><a href = "fourstars.html"> &#9734&#9734&#9734&#9734 </a></li>
-      <li><a href = "threestars.html"> &#9734&#9734&#9734 </a></li>
-      <li><a href = "twostars.html"> &#9734&#9734 </a></li>
-      <li><a href = "onestar.html"> &#9734 </a></li>
+      <li class="drop1">
+        <a href="crime.html" class="drop2">Crime</a>
+        <div class="drop3">
+          <a href="movies\pulpfiction.html">Pulp Fiction</a>
+          <a href="movies\babydriver.html">Baby Driver</a>
+          <a href="movies\trueromance.html">True Romance</a>
+        </div>
+      </li>
+      <li class="drop1">
+        <a href="thriller.html" class="drop2">Thriller</a>
+        <div class="drop3">
+          <a href="movies\push.html">Push</a>
+          <a href="movies\nerve.html">Nerve</a>
+          <a href="movies\goodtime.html">Good Time</a>
+        </div>
+      </li>
     </ul>
     </div>
+
+    <p> Included in this website is a complete database of the filmography I have personally consumed and enjoyed. As both a film student and movie buff, I have had the opportunity to garner an extensive watch list of both classic and modern cinema and as a result, the website can prove to be a valuable resource for those who, like me, have a vested interest in motion pictures.</p>
+
+    <img src="http://ali8.cs321.simmons.edu/pictures/thisistheend.jpg">
+    <img src="http://ali8.cs321.simmons.edu/pictures/theinterview.jpg">
+    <img src="http://ali8.cs321.simmons.edu/pictures/trueromance.jpg">
+    <img src="http://ali8.cs321.simmons.edu/pictures/push.jpg">
+    <img src="http://ali8.cs321.simmons.edu/pictures/nerve.jpg">
+    <img src="http://ali8.cs321.simmons.edu/pictures/goodtime.jpg">
+    <img src="http://ali8.cs321.simmons.edu/pictures/pulpfiction.jpg">
+    <img src="http://ali8.cs321.simmons.edu/pictures/thedictator.jpg">
+    <img src="http://ali8.cs321.simmons.edu/pictures/amelie.jpg">
+    <img src="http://ali8.cs321.simmons.edu/pictures/bladesofglory.jpg">
+    <img src="http://ali8.cs321.simmons.edu/pictures/roughnight.jpg">
+    <img src="http://ali8.cs321.simmons.edu/pictures/thedukesofhazzard.jpg">
+    <img src="http://ali8.cs321.simmons.edu/pictures/popstar.jpg">
+    <img src="http://ali8.cs321.simmons.edu/pictures/21jumpstreet.jpg">
+    <img src="http://ali8.cs321.simmons.edu/pictures/thehangover.jpg">
+    <img src="http://ali8.cs321.simmons.edu/pictures/500daysofsummer.jpg">
+    <img src="http://ali8.cs321.simmons.edu/pictures/thewolfofwallstreet.jpg">
+    <img src="http://ali8.cs321.simmons.edu/pictures/panslabyrinth.jpg">
+    <img src="http://ali8.cs321.simmons.edu/pictures/underthetuscansun.jpg">
+    <img src="http://ali8.cs321.simmons.edu/pictures/50firstdates.jpg">
+
+    <footer>
+      <div class="text">
+        <h6> Melat's Film Guide - CS321 - Assignment 2</h6>
+      </div>
+    </footer>
 
   </body>
 
