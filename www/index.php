9af73c3569991a5f48dc5a6d1e1f3e9c2f710e2b
Melat <49922671+MelatAssefa@users.noreply.github.com>
2019-09-25 17:22:42 -0400

    Update index.php

--- www/index.php
+++ www/index.php
@@ -65,26 +65,26 @@
 
     <p> Included in this website is a complete database of the filmography I have personally consumed and enjoyed. As both a film student and movie buff, I have had the opportunity to garner an extensive watch list of both classic and modern cinema and as a result, the website can prove to be a valuable resource for those who, like me, have a vested interest in motion pictures.</p>
 
-    <img src="http://ali8.cs321.simmons.edu/CS321/www/images/thisistheend.jpg">
-    <img src="http://ali8.cs321.simmons.edu/CS321/www/images/theinterview.jpg">
-    <img src="http://ali8.cs321.simmons.edu/CS321/www/images/trueromance.jpg">
-    <img src="http://ali8.cs321.simmons.edu/CS321/www/images/push.jpg">
-    <img src="http://ali8.cs321.simmons.edu/CS321/www/images/nerve.jpg">
-    <img src="http://ali8.cs321.simmons.edu/CS321/www/images/goodtime.jpg">
-    <img src="http://ali8.cs321.simmons.edu/CS321/www/images/pulpfiction.jpg">
-    <img src="http://ali8.cs321.simmons.edu/CS321/www/images/thedictator.jpg">
-    <img src="http://ali8.cs321.simmons.edu/CS321/www/images/amelie.jpg">
-    <img src="http://ali8.cs321.simmons.edu/CS321/www/images/bladesofglory.jpg">
-    <img src="http://ali8.cs321.simmons.edu/CS321/www/images/roughnight.jpg">
-    <img src="http://ali8.cs321.simmons.edu/CS321/www/images/thedukesofhazzard.jpg">
-    <img src="http://ali8.cs321.simmons.edu/CS321/www/images/popstar.jpg">
-    <img src="http://ali8.cs321.simmons.edu/CS321/www/images/21jumpstreet.jpg">
-    <img src="http://ali8.cs321.simmons.edu/CS321/www/images/thehangover.jpg">
-    <img src="http://ali8.cs321.simmons.edu/CS321/www/images/500daysofsummer.jpg">
-    <img src="http://ali8.cs321.simmons.edu/CS321/www/images/thewolfofwallstreet.jpg">
-    <img src="http://ali8.cs321.simmons.edu/CS321/www/images/panslabyrinth.jpg">
-    <img src="http://ali8.cs321.simmons.edu/CS321/www/images/underthetuscansun.jpg">
-    <img src="http://ali8.cs321.simmons.edu/CS321/www/images/50firstdates.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/thisistheend.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/theinterview.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/trueromance.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/push.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/nerve.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/goodtime.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/pulpfiction.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/thedictator.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/amelie.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/bladesofglory.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/roughnight.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/thedukesofhazzard.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/popstar.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/21jumpstreet.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/thehangover.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/500daysofsummer.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/thewolfofwallstreet.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/panslabyrinth.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/underthetuscansun.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/50firstdates.jpg">
 
     <footer>
       <div class="text">
