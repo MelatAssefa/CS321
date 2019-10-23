d8f3a2e865af3c759641ca31dfe145475b52424f
Melat <melat.ali@simmons.edu>
2019-10-23 19:15:52 -0400

    Done

--- www/comedy.php
+++ www/comedy.php
@@ -13,9 +13,30 @@
   <body>
 
     <div class="header">
-      <h1 class="title2">Comedy</h1>
+    <img src="http://ali8.cs321.simmons.edu/images/thisistheend.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/theinterview.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/thedictator.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/bladesofglory.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/roughnight.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/thedukesofhazzard.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/popstar.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/21jumpstreet.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/thehangover.jpg">
+ 
+
+        <h1 class="title2">Comedy</h1>
     </div>
-    
+
+    <img src="http://ali8.cs321.simmons.edu/images/thisistheend.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/theinterview.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/thedictator.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/bladesofglory.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/roughnight.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/thedukesofhazzard.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/popstar.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/21jumpstreet.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/thehangover.jpg">
+ 
 
     <?php include 'bottom.php';?>
 
