175bdd8140f1f0b8457cef14f8f4dabc5b5dbe6d
Melat <melat.ali@simmons.edu>
2019-10-23 18:46:53 -0400

    Done

--- www/index.php
+++ www/index.php
@@ -11,6 +11,27 @@
   <body>
 
     <div class="header">
+            <img src="http://ali8.cs321.simmons.edu/images/thisistheend.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/theinterview.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/trueromance.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/push.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/nerve.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/goodtime.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/pulpfiction.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/thedictator.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/amelie.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/bladesofglory.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/roughnight.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/thedukesofhazzard.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/popstar.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/21jumpstreet.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/thehangover.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/500daysofsummer.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/thewolfofwallstreet.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/panslabyrinth.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/underthetuscansun.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/50firstdates.jpg">
+
         <h1 class="title">Melat's Film Guide</h1>
     </div>
 
