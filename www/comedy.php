d37959a544834a1c2a753c0f4dd269adddcc2817
Melat <melat.ali@simmons.edu>
2019-10-23 19:18:56 -0400

    Done

--- www/comedy.php
+++ www/comedy.php
@@ -14,14 +14,14 @@
 
     <div class="header">
     <img class="type2" src="http://ali8.cs321.simmons.edu/images/thisistheend.jpg">
-    <img src="http://ali8.cs321.simmons.edu/images/theinterview.jpg">
-    <img src="http://ali8.cs321.simmons.edu/images/thedictator.jpg">
-    <img src="http://ali8.cs321.simmons.edu/images/bladesofglory.jpg">
-    <img src="http://ali8.cs321.simmons.edu/images/roughnight.jpg">
-    <img src="http://ali8.cs321.simmons.edu/images/thedukesofhazzard.jpg">
-    <img src="http://ali8.cs321.simmons.edu/images/popstar.jpg">
-    <img src="http://ali8.cs321.simmons.edu/images/21jumpstreet.jpg">
-    <img src="http://ali8.cs321.simmons.edu/images/thehangover.jpg">
+    <img class="type2" src="http://ali8.cs321.simmons.edu/images/theinterview.jpg">
+    <img class="type2" src="http://ali8.cs321.simmons.edu/images/thedictator.jpg">
+    <img class="type2" src="http://ali8.cs321.simmons.edu/images/bladesofglory.jpg">
+    <img class="type2" src="http://ali8.cs321.simmons.edu/images/roughnight.jpg">
+    <img class="type2" src="http://ali8.cs321.simmons.edu/images/thedukesofhazzard.jpg">
+    <img class="type2" src="http://ali8.cs321.simmons.edu/images/popstar.jpg">
+    <img class="type2" src="http://ali8.cs321.simmons.edu/images/21jumpstreet.jpg">
+    <img class="type2" src="http://ali8.cs321.simmons.edu/images/thehangover.jpg">
  
 
         <h1 class="title2">Comedy</h1>
