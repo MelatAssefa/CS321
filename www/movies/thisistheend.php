ccd06057b2a275fc6f50cc11f6f44903f5d31741
Melat <melat.ali@simmons.edu>
2019-10-23 19:29:15 -0400

    Done

---
+++ www/movies/thisistheend.php
@@ -0,0 +1,27 @@
+<?php include '../top.php'; ?>
+
+<!doctype html>
+
+<html lang="en">
+       
+  <head>
+    <title>This is the End | Melat's Film Guide</title>
+    <meta charset=”utf-8”>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+
+  <body>
+
+    <div class="header">
+      <h1 class="title2">This is the End</h1>
+    </div>
+    
+    <iframe src="https://open.spotify.com/embed/playlist/64sPn4CkHsBYqQRaBkSx4B" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      
+    <img class="type3" src="http://ali8.cs321.simmons.edu/images/thisistheend.jpg">
+
+    <?php include 'bottom.php';?>
+
+  </body>
+
+</html>
