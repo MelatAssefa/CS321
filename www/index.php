2436abc76c46e3dec8e8f0a8f34acf700d567948
Melat <melat.ali@simmons.edu>
2019-10-23 18:38:29 -0400

    Done

--- www/index.php
+++ www/index.php
@@ -1,67 +1,13 @@
+<?php include 'top.php'; ?>
+
 <!doctype html>
 
 <html lang="en">
-
-  <head>
-
-    <!-- Author: Melat A Ali -->
-    <!-- Project: Assignment 3 -->
-
-    <title>Melat's Film Guide</title>
-    <meta charset=”utf-8”>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel='icon' href='http://ali8.cs321.simmons.edu/images/favicon.ico' type='image/x-icon'/ >
-
-    <!--Favicon: https://www.101computing.net/html-how-to-add-a-favicon/
-    https://www.iconfinder.com/icons/1298806/action_design_movie_process_round_icon-->
-
-  </head>
 
   <body>
 
     <div class="header">
         <h1 class="title">Melat's Film Guide</h1>
-    </div>
-
-    <div class="menu">
-      <ul>
-      <li><a href="try.php">Home</a></li>
-      <li class="drop1">
-        <a href="comedy.php" class="drop2">Comedy</a>
-        <div class="drop3">
-          <a href="thisistheend.php">This Is The End</a>
-          <a href="movies\theinterview.html">The Interview</a>
-          <a href="movies\thedictator.html">The Dictator</a>
-          <a href="movies\amelie.html">Amelie</a>
-        </div>
-      <li class="drop1">
-        <a href="drama.html" class="drop2">Drama</a>
-        <div class="drop3">
-          <a href="movies\500daysofsummer.html">500 Days of Summer</a>
-          <a href="movies\thewolfofwallstreet.html">The Wolf of Wall Street</a>
-          <a href="movies\panslabyrinth.html">Pan's Labyrinth</a>
-          <a href="movies\underthetuscansun.html">Under The Tuscan Sun</a>
-
-        </div>
-      </li>
-      <li class="drop1">
-        <a href="crime.html" class="drop2">Crime</a>
-        <div class="drop3">
-          <a href="movies\pulpfiction.html">Pulp Fiction</a>
-          <a href="movies\babydriver.html">Baby Driver</a>
-          <a href="movies\trueromance.html">True Romance</a>
-        </div>
-      </li>
-      <li class="drop1">
-        <a href="thriller.html" class="drop2">Thriller</a>
-        <div class="drop3">
-          <a href="movies\push.html">Push</a>
-          <a href="movies\nerve.html">Nerve</a>
-          <a href="movies\goodtime.html">Good Time</a>
-        </div>
-      </li>
-      <li><a href="contact.php">Contact Me</a></li>
-    </ul>
     </div>
 
     <p> Included in this website is a complete database of the filmography I have personally consumed and enjoyed. As both a film student and movie buff, I have had the opportunity to garner an extensive watch list of both classic and modern cinema and as a result, the website can prove to be a valuable resource for those who, like me, have a vested interest in motion pictures.</p>
@@ -87,11 +33,7 @@
     <img src="http://ali8.cs321.simmons.edu/images/underthetuscansun.jpg">
     <img src="http://ali8.cs321.simmons.edu/images/50firstdates.jpg">
 
-    <footer>
-      <h6> 
-        <img src="http://ali8.cs321.simmons.edu/images/favicon.png"> &copy 2019 CS321 | Melat's Film Guide 
-      </h6>
-    </footer>
+    <?php include 'bottom.php';?>
 
   </body>
 
