eb8716ebeaa2e70aee13b01886327e45c4bd9c3d
Melat <melat.ali@simmons.edu>
2019-10-23 18:35:36 -0400

    Done

--- www/thriller.php
+++ www/thriller.php
@@ -1,3 +1,18 @@
 <?php include 'top.php'; ?>
 
-<?php include 'bottom.php';?>
+<!doctype html>
+
+<html lang="en">
+
+  <body>
+
+    <div class="header">
+      <h1 class="title2">Thriller</h1>
+    </div>
+    
+
+    <?php include 'bottom.php';?>
+
+  </body>
+
+</html>
