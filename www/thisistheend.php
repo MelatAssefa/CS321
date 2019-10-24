dd5a3292d2bdce0e47fa4430b460297cad25fa1c
Melat <melat.ali@simmons.edu>
2019-10-23 20:20:01 -0400

    Done

---
+++ www/thisistheend.php
@@ -0,0 +1,33 @@
+<?php include 'top.php'; ?>
+
+<!doctype html>
+
+<html lang="en">
+       
+  <head>
+    <title>This is the End | Melat's Film Guide</title>
+  </head>
+
+  <body>
+
+    <div class="header">
+      <h1 class="title2">This is the End</h1>
+    </div>
+    
+    <iframe src="https://open.spotify.com/embed/playlist/64sPn4CkHsBYqQRaBkSx4B" 
+            width="300" height="360" frameborder="0" allowtransparency="true" 
+            allow="encrypted-media"></iframe>
+      
+    <img class="type3" src="http://ali8.cs321.simmons.edu/images/thisistheend.jpg">
+    
+    <iframe width="300" height="360" src="https://www.youtube.com/embed/kliQSsD_npo" 
+            frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen></iframe>
+    
+    <p>I guess if Michael Cera's gone, it's not a TOTAL loss.<p>
+
+    <?php include 'bottom.php';?>
+
+  </body>
+
+</html>
