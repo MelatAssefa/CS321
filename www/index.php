cd44a03bf4435c8bebcd59efd7783fde181d0d85
Melat <melat.ali@simmons.edu>
2019-11-06 19:16:56 -0500

    Resized Images

--- www/index.php
+++ www/index.php
@@ -6,6 +6,7 @@
     
     <head>
         <title>Melat's Film Guide</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
     </head>
 
   <body>
@@ -32,12 +33,11 @@
     <img src="http://ali8.cs321.simmons.edu/images/underthetuscansun.jpg">
 
         <h1 class="title">Melat's Film Guide</h1>
-    </div>
-
-    <img src="http://ali8.cs321.simmons.edu/images/thisistheend.jpg">
-    <img src="http://ali8.cs321.simmons.edu/images/theinterview.jpg">
-    <img src="http://ali8.cs321.simmons.edu/images/trueromance.jpg">
-    <img src="http://ali8.cs321.simmons.edu/images/push.jpg">
+        
+    <img src="http://ali8.cs321.simmons.edu/images/babydriver.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/circle.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/onceuponatimeinhollywood.jpg">
+    <img src="http://ali8.cs321.simmons.edu/images/50firstdates.jpg">
     <img src="http://ali8.cs321.simmons.edu/images/nerve.jpg">
     <img src="http://ali8.cs321.simmons.edu/images/goodtime.jpg">
     <img src="http://ali8.cs321.simmons.edu/images/pulpfiction.jpg">
@@ -53,7 +53,7 @@
     <img src="http://ali8.cs321.simmons.edu/images/thewolfofwallstreet.jpg">
     <img src="http://ali8.cs321.simmons.edu/images/panslabyrinth.jpg">
     <img src="http://ali8.cs321.simmons.edu/images/underthetuscansun.jpg">
-    <img src="http://ali8.cs321.simmons.edu/images/50firstdates.jpg">
+    </div>
 
     <?php include 'bottom.php';?>
 
--- www/top.php
+++ www/top.php
@@ -8,51 +8,48 @@
     <link rel='icon' href='http://ali8.cs321.simmons.edu/images/favicon.ico' type='image/x-icon'/ >
          
     <!--Favicon: https://www.iconfinder.com/icons/1298806/action_design_movie_process_round_icon-->
+    
+  <nav>    
+     <div class="menu">
+        <ul>
+        <li><a href="http://ali8.cs321.simmons.edu/">Home</a></li>
+        <li class="drop1">
+          <a href="http://ali8.cs321.simmons.edu/comedy.php" class="drop2">Comedy</a>
+          <div class="drop3">
+            <a href="thisistheend.php">This Is The End</a>
+            <a href="movies\theinterview.php">The Interview</a>
+            <a href="movies\thedictator.php">The Dictator</a>
+            <a href="movies\amelie.php">Amelie</a>
+          </div>
+        <li class="drop1">
+          <a href="http://ali8.cs321.simmons.edu/drama.php" class="drop2">Drama</a>
+          <div class="drop3">
+            <a href="movies\500daysofsummer.php">500 Days of Summer</a>
+            <a href="movies\thewolfofwallstreet.php">The Wolf of Wall Street</a>
+            <a href="movies\panslabyrinth.php">Pan's Labyrinth</a>
+            <a href="movies\underthetuscansun.php">Under The Tuscan Sun</a>
 
+          </div>
+        </li>
+        <li class="drop1">
+          <a href="http://ali8.cs321.simmons.edu/crime.php" class="drop2">Crime</a>
+          <div class="drop3">
+            <a href="movies\pulpfiction.php">Pulp Fiction</a>
+            <a href="movies\babydriver.php">Baby Driver</a>
+            <a href="movies\trueromance.php">True Romance</a>
+          </div>
+        </li>
+        <li class="drop1">
+          <a href="http://ali8.cs321.simmons.edu/thriller.php" class="drop2">Thriller</a>
+          <div class="drop3">
+            <a href="movies\push.php">Push</a>
+            <a href="movies\nerve.php">Nerve</a>
+            <a href="movies\goodtime.php">Good Time</a>
+          </div>
+        </li>
+        <li><a href="http://ali8.cs321.simmons.edu/contact.php">Contact Me</a></li>
+      </ul>
+      </div>
+  </nav>
   </head>
-
-  <body>
-
-    <div class="menu">
-      <ul>
-      <li><a href="http://ali8.cs321.simmons.edu/">Home</a></li>
-      <li class="drop1">
-        <a href="http://ali8.cs321.simmons.edu/comedy.php" class="drop2">Comedy</a>
-        <div class="drop3">
-          <a href="thisistheend.php">This Is The End</a>
-          <a href="movies\theinterview.php">The Interview</a>
-          <a href="movies\thedictator.php">The Dictator</a>
-          <a href="movies\amelie.php">Amelie</a>
-        </div>
-      <li class="drop1">
-        <a href="http://ali8.cs321.simmons.edu/drama.php" class="drop2">Drama</a>
-        <div class="drop3">
-          <a href="movies\500daysofsummer.php">500 Days of Summer</a>
-          <a href="movies\thewolfofwallstreet.php">The Wolf of Wall Street</a>
-          <a href="movies\panslabyrinth.php">Pan's Labyrinth</a>
-          <a href="movies\underthetuscansun.php">Under The Tuscan Sun</a>
-
-        </div>
-      </li>
-      <li class="drop1">
-        <a href="http://ali8.cs321.simmons.edu/crime.php" class="drop2">Crime</a>
-        <div class="drop3">
-          <a href="movies\pulpfiction.php">Pulp Fiction</a>
-          <a href="movies\babydriver.php">Baby Driver</a>
-          <a href="movies\trueromance.php">True Romance</a>
-        </div>
-      </li>
-      <li class="drop1">
-        <a href="http://ali8.cs321.simmons.edu/thriller.php" class="drop2">Thriller</a>
-        <div class="drop3">
-          <a href="movies\push.php">Push</a>
-          <a href="movies\nerve.php">Nerve</a>
-          <a href="movies\goodtime.php">Good Time</a>
-        </div>
-      </li>
-      <li><a href="http://ali8.cs321.simmons.edu/contact.php">Contact Me</a></li>
-    </ul>
-    </div>
-      
-  </body>
 </html>
