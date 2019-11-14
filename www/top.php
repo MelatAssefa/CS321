f2ad72bde21e68ece5f756707ca180ff2a19207e
Melat <melat.ali@simmons.edu>
2019-11-13 19:31:22 -0500

    Fixed

--- www/top.php
+++ www/top.php
@@ -51,5 +51,15 @@
       </ul>
       </div>
   </nav>
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-152527272-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-152527272-1');
+</script>
   </head>
+
 </html>
--- www/tryingxml.php
+++ www/tryingxml.php
@@ -1,6 +1,14 @@
+<?php
+if(isset($_POST['buttonImport'])) {
+    copy($_FILES['xmlFile']['tmp_name'],
+            'data/'.$_FILES['xmlFile']['tmp_name']);
+}
+?>
 <form method="post" enctype="multipart/form-data">
-    XML File <input tupe="file" name="xmlFile">
+    XML File <input type="file" name="xmlFile">
     <br>
     <input type="submit" value="Import" name="buttonImport">
+    
+   
 </form>
  
